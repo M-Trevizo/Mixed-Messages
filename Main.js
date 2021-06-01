2335f82b0bd0537a18657180882723e6d8c2188a
Mikey <michael.trevizo93@gmail.com>
2021-06-01 16:16:38 -0700

    First commit, start of project

---
+++ Main.js
@@ -0,0 +1,6 @@
+const adjective = [];
+const verb = [];
+const today = new Date();
+let dayOfWeek = today.getDay();
+
+//ToDo: The rest of it.
---
+++ README.md
@@ -0,0 +1,3 @@
+# Mixed Messages Portfolio Project
+
+This program will generate a new motivational message each time the program is run.
